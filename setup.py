e6f0cbfd65c40c15126e6ecec2688f5a92c38a82
innat <innat.dev@gmail.com>
2023-10-06 17:38:35 +0600

    add: requirement.txt, setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'videomae',
+  packages = find_packages(exclude=['notebooks', 'assets']),
+  version = '1.0.0',
+  license='MIT',
+  description = 'VideoMAE, A Video Masked Autoencoders - Keras',
+  long_description=open('README.md').read(),
+  long_description_content_type='text/markdown',
+  author = 'Mohammed Innat',
+  author_email = 'innat.dev@gmail.com',
+  url = 'https://github.com/innat/DOLG-TensorFlow',
+  keywords = [
+    'deep learning',
+    'image retrieval',
+    'image recognition'
+  ],
+  install_requires=[
+    'opencv-python>=4.1.2',
+    'tensorflow>=2.12',
+  ],
+  setup_requires=[
+    'pytest-runner',
+  ],
+  tests_require=[
+    'pytest'
+  ],
+  classifiers=[
+    'Development Status :: 1 - Planning',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
